da2028bdcb7908f6f15f21b00ad7c3ed5549601c
Natanael Bezerra <nsbbezerra@hotmail.com>
2021-09-21 15:35:38 -0300

    configure cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from "express";
 import { router } from "./routes";
 import { resolve } from "path";
+import cors from "cors";
 
 interface HTTPError extends Error {
   status?: number;
@@ -8,6 +9,7 @@ interface HTTPError extends Error {
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 app.use((error: HTTPError, req: Request, res: Response, next: NextFunction) => {
