89f5aafff4bc290b8493d75666f77a40e466aeb9
Natanael <natanael@pop-os.localdomain>
2021-09-29 15:37:12 -0300

    add tsconfig

--- src/database/database.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const connection = "mongodb://localhost:27017/prefeitura";
-
-mongoose.connect(connection);
-mongoose.Promise = global.Promise;
-
-export { mongoose };
