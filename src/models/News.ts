63aadf6631721154faf5e931ed47013cfd3fe185
Natanael Bezerra <nsbbezerra@hotmail.com>
2021-10-28 07:17:28 -0300

    remove required of news fields

--- src/models/News.ts
+++ src/models/News.ts
@@ -4,17 +4,17 @@ const model = mongoose.model;
 
 interface INews {
   title: string;
-  resume: string;
-  author: string;
+  resume?: string;
+  author?: string;
   date: Date;
   image: string;
-  imageCopy: string;
+  imageCopy?: string;
   text: string;
   galery?: IImages[];
   month: string;
   year: number;
   created_at: Date;
-  tag: string;
+  tag?: string;
 }
 
 interface IImages {
@@ -23,17 +23,17 @@ interface IImages {
 
 const schema = new Schema<INews>({
   title: { type: String, required: true },
-  resume: { type: String, required: true },
-  author: { type: String, required: true },
+  resume: { type: String, required: false },
+  author: { type: String, required: false },
   date: { type: Date, required: true },
   image: { type: String, required: true },
-  imageCopy: { type: String, required: true },
+  imageCopy: { type: String, required: false },
   text: { type: String, required: true },
   month: { type: String, required: true },
   year: { type: Number, required: true },
   galery: [{ image: { type: String } }],
   created_at: { type: Date, required: false },
-  tag: { type: String, required: true },
+  tag: { type: String, required: false },
 });
 
 schema.pre("save", function (next) {
